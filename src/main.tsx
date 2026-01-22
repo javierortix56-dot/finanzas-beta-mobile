bc50dde228cefff95998af03c3de726ff2ce2429
javierortix56-dot <javierortix56@gmail.com>
2026-01-22 15:37:50 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App' // Corrección: Quitamos el ".tsx" del final
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

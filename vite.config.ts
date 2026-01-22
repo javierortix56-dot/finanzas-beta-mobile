9e1b9d28fec20473e0fd4336d44e08b91445de6c
javierortix56-dot <javierortix56@gmail.com>
2026-01-22 15:16:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/finanzas-beta-mobile/', // Debe coincidir con el nombre de tu repo
+  base: '/finanzas-beta-mobile/', 
 })
